c63442ed5f25b68a5f6b7507dce4cc2021eeae4f
Bruno Marcato <marcato@MacBook-Air-de-UTFPR-Administrador.local>
2023-10-12 13:51:11 -0300

    functions to train and apply style transfer/some utils functions

---
+++ style_transfer.py
@@ -0,0 +1,13 @@
+
+def apply_style_transfer(content, style):
+    pass
+
+def train_style_transfer(content, style):
+    # Save pytorch model to onnx format to read in opencv readNetFromONNX
+    pass
+
+if __name__ == '__main__':
+    content = None
+    style = None
+    
+    train_style_transfer(content, style)
